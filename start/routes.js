9aab8c9bec64e4d074192ffc679ef45684182d3f
Voitila Pereira de Araujo <voitila.araujo@QFOODS.COM>
2022-05-19 17:57:41 -0300

    pdv

--- start/routes.js
+++ start/routes.js
@@ -136,6 +136,7 @@ Route.delete("/coletas/detalhes/apagar/:EquiCod/:AnxId/:PdvId/:FfmSeq", "WEB/Con
 
 //Pontos de Venda
 Route.get("/pontosdevenda", "WEB/PontosDeVendaController.Show").middleware('jwt'); //retorna todos os pontos de venda do franqueado
+Route.get("/pontosdevenda/info/:pdvid/:anxid/:type", "WEB/PontosDeVendaController.See").middleware('jwt'); //retorna todos os pontos de venda do franqueado
 Route.put("/pontosdevenda/inativar", "WEB/PontosDeVendaController.InativPDV").middleware('jwt'); //inativa pdv
 Route.put("/pontosdevenda/atualizar", "WEB/PontosDeVendaController.Update").middleware('jwt'); //atualiza dados do pdv
 
