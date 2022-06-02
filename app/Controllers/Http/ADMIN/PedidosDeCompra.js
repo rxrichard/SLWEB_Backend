63bcd2373d3ace00706628db8a4b27d8f6c31089
Voitila Pereira de Araujo <voitila.araujo@QFOODS.COM>
2022-06-02 18:04:39 -0300

    work 2.6

--- app/Controllers/Http/ADMIN/PedidosDeCompra.js
+++ app/Controllers/Http/ADMIN/PedidosDeCompra.js
@@ -20,7 +20,16 @@ class PedidosDeCompra {
         throw new Errow('Usuário não permitido')
       }
 
-      const pedidosDeCompraEmAberto = await Database.raw(QUERY_PEDIDOS_DE_COMPRA_EM_ABERTO)
+      let pedidosDeCompraEmAberto = await Database.raw(QUERY_PEDIDOS_DE_COMPRA_EM_ABERTO)
+
+      for (let i = 0; i < pedidosDeCompraEmAberto.length; i++) {
+        let d = await Database.raw("select PedidoItemID, CodigoProduto, Produto, QtdeVendida, PrecoUnitarioLiquido, PrecoTotal from dbo.PedidosVenda left join dbo.Produtos on dbo.PedidosVenda.CodigoProduto = dbo.Produtos.ProdId where Filial = '0201' and PedidoID = ? order by PedidoItemID ASC", [pedidosDeCompraEmAberto[i].PedidoID])
+
+        pedidosDeCompraEmAberto[i] = {
+          ...pedidosDeCompraEmAberto[i],
+          Detalhes: d
+        }
+      }
 
       response.status(200).send({
         Pedidos: pedidosDeCompraEmAberto
@@ -40,4 +49,4 @@ class PedidosDeCompra {
 
 module.exports = PedidosDeCompra;
 
-const QUERY_PEDIDOS_DE_COMPRA_EM_ABERTO = "SELECT dbo.FilialEntidadeGrVenda.UF, dbo.PedidosVenda.Filial, dbo.PedidosVenda.CodigoCliente, dbo.PedidosVenda.LojaCliente, dbo.PedidosVenda.PedidoID, Count(dbo.PedidosVenda.PedidoItemID) AS ContarDePedidoItemID, dbo.PedidosVenda.DataCriacao, dbo.PedidosVenda.TipOp, Max(dbo.PedidosVenda.TES) AS MáxDeTES, Sum(dbo.PedidosVenda.PrecoTotal) AS SomaDePrecoTotal FROM dbo.PedidosVenda INNER JOIN dbo.FilialEntidadeGrVenda ON dbo.PedidosVenda.Filial = dbo.FilialEntidadeGrVenda.M0_CODFIL WHERE (((dbo.PedidosVenda.CodigoTotvs) Is Null)) GROUP BY dbo.FilialEntidadeGrVenda.UF, dbo.PedidosVenda.STATUS, dbo.FilialEntidadeGrVenda.NASAJON, dbo.PedidosVenda.Filial, dbo.PedidosVenda.CodigoCliente, dbo.PedidosVenda.LojaCliente, dbo.PedidosVenda.PedidoID, dbo.PedidosVenda.DataCriacao, dbo.PedidosVenda.DataIntegracao, dbo.PedidosVenda.TipOp, dbo.PedidosVenda.SERIE, dbo.PedidosVenda.EMISS HAVING (((dbo.PedidosVenda.STATUS) Is Null and dbo.FilialEntidadeGrVenda.NASAJON = 'N')) ORDER BY dbo.PedidosVenda.DataCriacao DESC"
+const QUERY_PEDIDOS_DE_COMPRA_EM_ABERTO = "SELECT dbo.FilialEntidadeGrVenda.UF, dbo.PedidosVenda.Filial, dbo.PedidosVenda.CodigoCliente, dbo.PedidosVenda.LojaCliente, dbo.PedidosVenda.PedidoID, dbo.PedidosVenda.MsgBO, Count(dbo.PedidosVenda.PedidoItemID) AS ContarDePedidoItemID, dbo.PedidosVenda.DataCriacao, dbo.PedidosVenda.TipOp, Max(dbo.PedidosVenda.TES) AS MáxDeTES, Sum(dbo.PedidosVenda.PrecoTotal) AS SomaDePrecoTotal FROM dbo.PedidosVenda INNER JOIN dbo.FilialEntidadeGrVenda ON dbo.PedidosVenda.Filial = dbo.FilialEntidadeGrVenda.M0_CODFIL WHERE (((dbo.PedidosVenda.CodigoTotvs) Is Null)) GROUP BY dbo.FilialEntidadeGrVenda.UF, dbo.PedidosVenda.STATUS, dbo.FilialEntidadeGrVenda.NASAJON, dbo.PedidosVenda.Filial, dbo.PedidosVenda.CodigoCliente, dbo.PedidosVenda.LojaCliente, dbo.PedidosVenda.PedidoID, dbo.PedidosVenda.MsgBO, dbo.PedidosVenda.DataCriacao, dbo.PedidosVenda.DataIntegracao, dbo.PedidosVenda.TipOp, dbo.PedidosVenda.SERIE, dbo.PedidosVenda.EMISS HAVING (((dbo.PedidosVenda.STATUS) Is Null and dbo.FilialEntidadeGrVenda.NASAJON = 'N')) ORDER BY dbo.PedidosVenda.DataCriacao DESC"
